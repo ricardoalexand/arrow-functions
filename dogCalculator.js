77e1519c0c08d15ffb5a367fe83e24a551879e8a
ricardoalexand <126792060+ricardoalexand@users.noreply.github.com>
2024-01-17 22:42:56 +0000

    new changes

--- dogCalculator.js
+++ dogCalculator.js
@@ -5,15 +5,14 @@ let result = document.querySelector(".result");
 
 
 // 1. Convert to an arrow function.
-function calculateDoggieAge(humanAge) {
-
+const calculateDoggieAge = (humanAge) => {
   // 2. Declare a doggieAge variable and set it equal to the humanAge parameter multiplied by 7.
-  
+  let doggieAge = humanAge * 7;
 
   // 3. Update the result div's inner html to display the dog's age. Use string interpolation to create the message: "Your doggie is _________ years old in human years!"
 
   
-  
+  result.innerHTML = 'Your doggy is ${doggieAge} years old in human years'
 }
 
 
@@ -21,10 +20,10 @@ function calculateDoggieAge(humanAge) {
 equalsButton.addEventListener("click", () => {
 
   // 4. Declare a userInput variable and set it equal to the value of the input. This needs to be a number, not a string! Be sure to place that inside our Number() method!
-  
+  let userInput = Number(inupt.value)
 
   // 5. Call the calculateDoggieAge function. Be sure to include the userInput variable as the parameter!
-
+calculateDoggieAge(userInput)
 
 })
 
--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function fourSquared() {
-
+    
+  let num = 4;
     // 1. Declare a variable called num and set it equal to 4.
     
-    
+    let numSquared = num * num;
     // 2. Declare a variable called numSquared and set it equal to num multiplied by itself.
     
     
-    
+    return numSquared
     // 3. Return numSquared
     
     
@@ -14,8 +15,8 @@ function fourSquared() {
     
     
     // 4. Declare a variable called answer and set it equal to a function call of fourSquared.
-    
+    let answer = fourSquared();
     
     
     // 5. Console.log the answer variable. You should see the number 16 in the console.
-    
+    console.log(answer);
